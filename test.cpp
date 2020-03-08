2a2e4701e35529d2ece4f9c8b2acb4b790ad9634
Diyuse <daniel.yan.wu@gmail.com>
2020-03-07 21:08:52 -0500

    First commit

---
+++ test.cpp
@@ -0,0 +1,22 @@
+#include <cstdlib> // Import c standard library
+#include <iostream> // Import read and write data
+#include <string> // Import working with strings
+#include <limits> // Import to get min and max values of datatypes
+#include <vector> // Import vectors
+#include <sstream> // Import string streams
+#include <numeric> // Import working with sequences of values
+#include <ctime> // Import working with time
+#include <cmath> // Import math functions
+
+using namespace std;
+
+int main(int argc, char** argv) {
+
+	cout << "Hello World!" << endl; // << is the stream insertion operator
+	// cout is the stream, endl issues a new line and writes to the console
+
+	// Written like this if there was to std namespace
+//	std::cout << "Hello World!" << endl;
+
+	return 0;
+}
