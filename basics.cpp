c67ebc87f60527a31a0e759119e4551200c613ac
Diyuse <daniel.yan.wu@gmail.com>
2020-03-08 14:13:46 -0400

    Added test on strings

--- basics.cpp
+++ basics.cpp
@@ -25,5 +25,29 @@ int main(int argc, char** argv) {
 	// Written like this if there was to std namespace
 //	std::cout << "Hello World!" << endl;
 
+    bool married = false;
+    char myGrade = 'A';
+
+    // Showing min and max of integers, can do the same for floating point
+    cout << "Min unsigned short int " << numeric_limits<unsigned short int>::min() << endl;
+    cout << "Max unsigned short int " << numeric_limits<unsigned short int>::max() << endl;
+    cout << "Min short int " << numeric_limits<short int>::min() << endl;
+    cout << "Max short int " << numeric_limits<short int>::max() << endl;
+    cout << "Min int " << numeric_limits<int>::min() << endl;
+    cout << "Max int " << numeric_limits<int>::max() << endl;
+    cout << "Min long " << numeric_limits<long>::min() << endl;
+    cout << "Max long " << numeric_limits<long>::max() << endl;
+
+    float f1 = 1.1111111; // floats are only accurate to 6 digits
+    float f2 = 1.1111111;
+
+    printf("Sum = %.16f\n", (f1 + f2)); // format a print
+
+    auto whatAmi = true; // automatically assign type, here it will be set to bool
+
+    cout << "int Byte : " << sizeof(int) << endl; // sizeof returns how many bytes are used
+
+    printf("%c %d %5d %.3f %s\n", 'A', 10, 5, 3.1234, "Hi"); // %5d puts 5 spaces to the left of the int
+
 	return 0;
 }
---
+++ strings.cpp
@@ -0,0 +1,23 @@
+#include <cstdlib> // Import c standard library
+#include <iostream> // Import read and write data
+#include <string> // Import working with strings
+#include <limits> // Import to get min and max values of datatypes
+#include <vector> // Import vectors
+#include <sstream> // Import string streams
+#include <numeric> // Import working with sequences of values
+#include <ctime> // Import working with time
+#include <cmath> // Import math functions
+
+using namespace std;
+
+int main(int argc, char** argv) {
+    string q1 = "Enter a Number : "; // string declaration with or without field
+    string num1, num2;
+    cout << q1; // print q1
+    cin >> num1; // get an input and num1 points to the input
+    cout << "Enter Another Number : "; // print the string directly
+    cin >> num2;
+    int nNum1 = stoi(num1); // stoi converts string to integer, simiarly stof converts string to float
+    int nNum2 = stoi(num2);
+    printf("%d + %d = %d\n", nNum1, nNum2, (nNum1 + nNum2));
+}
