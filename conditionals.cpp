e36bf07968fd10ea2dbb1f5bdda28a92ab35a266
Diyuse <daniel.yan.wu@gmail.com>
2020-03-08 21:29:49 -0400

    Added test on conditionals

---
+++ conditionals.cpp
@@ -0,0 +1,39 @@
+#include <cstdlib> // Import c standard library
+#include <iostream> // Import read and write data
+#include <string> // Import working with strings
+#include <limits> // Import to get min and max values of datatypes
+#include <vector> // Import vectors
+#include <sstream> // Import string streams
+#include <numeric> // Import working with sequences of values
+#include <ctime> // Import working with time
+#include <cmath> // Import math functions
+
+using namespace std;
+
+int main(int argc, char** argv) {
+    // Conditional Operators: > < >= <= == !=
+    // Logical Operators: && || !
+
+    string sAge;
+    cout << "Enter your Age: ";
+    cin >> sAge;
+    int nAge = stoi(sAge);
+
+    if ((nAge >= 1) && (nAge <= 18)){
+        cout << "Important Birthday" << endl;
+    } else if ((nAge == 21) || (nAge == 50)){
+        cout << "Important Birthday" << endl;
+    } else if (nAge >= 65){
+        cout << "Important Birthday" << endl;
+    } else {
+        cout << "Not an Important Birthday" << endl;
+    }
+
+    //Ternary operator
+    int age18 = 18;
+    bool canIVote = (age18 >= 18) ? true : false;
+    cout.setf(ios::boolalpha); // To print true or false instead of 1 or 0
+    cout << "John Vote: " << canIVote << endl;
+
+    return 0;
+}
--- strings.cpp
+++ strings.cpp
@@ -11,13 +11,21 @@
 using namespace std;
 
 int main(int argc, char** argv) {
-    string q1 = "Enter a Number : "; // string declaration with or without field
+    string q1 = "Enter a Number: "; // string declaration with or without field
     string num1, num2;
     cout << q1; // print q1
     cin >> num1; // get an input and num1 points to the input
-    cout << "Enter Another Number : "; // print the string directly
+    cout << "Enter Another Number: "; // print the string directly
     cin >> num2;
     int nNum1 = stoi(num1); // stoi converts string to integer, simiarly stof converts string to float
     int nNum2 = stoi(num2);
+
+    // Math operators
     printf("%d + %d = %d\n", nNum1, nNum2, (nNum1 + nNum2));
+    printf("%d - %d = %d\n", nNum1, nNum2, (nNum1 - nNum2));
+    printf("%d * %d = %d\n", nNum1, nNum2, (nNum1 * nNum2));
+    printf("%d / %d = %d\n", nNum1, nNum2, (nNum1 / nNum2));
+    printf("%d %% %d = %d\n", nNum1, nNum2, (nNum1 % nNum2));
+
+    return 0;
 }
