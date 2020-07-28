612c5531ad5be8daaf03a9e93e78896b39454dca
Diyuse <daniel.yan.wu@gmail.com>
2020-07-28 14:49:55 -0400

    Changed folder structure

---
+++ Source Files/loops.cpp
@@ -0,0 +1,15 @@
+#include <cstdlib> // Import c standard library
+#include <iostream> // Import read and write data
+#include <string> // Import working with strings
+#include <limits> // Import to get min and max values of datatypes
+#include <vector> // Import vectors
+#include <sstream> // Import string streams
+#include <numeric> // Import working with sequences of values
+#include <ctime> // Import working with time
+#include <cmath> // Import math functions
+
+using namespace std;
+
+int main(int argc, char** argv) {
+    return 0;
+}
